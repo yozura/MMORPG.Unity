6d2b5e4b7bb6755c88f841a8996af3d4440e0bba
yozura <savedelove@naver.com>
2020-09-20 16:35:54 +0900

    싱글톤 패턴과 컴포넌트 패턴
    20/09/20 싱글톤 패턴과 컴포넌트 패턴에 대해 배움.

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    // 싱글턴 패턴
+    static Managers s_instance; // 유일성이 보장된다.
+    public static Managers Instance { get { init(); return s_instance; } } // 유일한 매니저를 가져온다.
+
+    void Start()
+    {
+        init();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    static void init()
+    {
+        if(s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if(go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+
+    }
+}

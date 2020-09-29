a0e08ada9e7f9c147587367669e9503768f3cf12
yozura <savedelove@naver.com>
2020-09-29 17:22:48 +0900

    0929_필요없는 코드 제거

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -1,17 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] Define.CameraMode _mode = Define.CameraMode.QuarterView;
     [SerializeField] Vector3 _delta = new Vector3(0.0f, 6.0f, -5.0f);
     [SerializeField] GameObject _player = null;
-
-    void Start()
-    {   
-            
-    }
 
     void LateUpdate()
     {
--- Assets/Scripts/Utlis/Define.cs
+++ Assets/Scripts/Utlis/Define.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Define
+﻿public class Define
 {
     public enum UIEvent
     {
